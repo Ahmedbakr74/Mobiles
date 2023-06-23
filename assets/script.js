b582778111d801cc1800021d3c21dae449cf69e1
Ahmed Bakr <117715752+Ahmedbakr74@users.noreply.github.com>
2023-06-23 16:27:15 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('body');
+function phone(mobile){
+    img.src = mobile;
+
+}
+function colors(color){
+    container.style.background = color;
+}
